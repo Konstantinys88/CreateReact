1e1fb124155f94d5ea27bf9b213f55a853c4e553
Konstantin0 <maschkovc@yandex.ru>
2023-05-23 14:30:02 +0300

    123. Элементы и компоненты

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,59 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const Header = () => {
+	return <h2>Hello Egor!</h2>
+}
+
+// const Field = () => {
+//   const holder = "Enter here";
+//   const styledFild = {
+//     width: '300px',
+//     color: 'red'
+//   }
+//   return <input type="text" 
+// 				placeholder={holder} 
+// 				style={styledFild}/>
+// }
+
+class Field extends React.Component {
+	render() {
+		const holder = "Enter here";
+		const styledFild = {
+			width: '300px',
+			color: 'red'
+		};
+		return <input type="text"
+			placeholder={holder}
+			style={styledFild} />
+	}
+
+}
+
+function Btn() {
+	const text = "Log in";
+	const logged = false;
+
+	return <button>{logged ? "Enter" : text}</button>
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+	return (
+		<div className="App">
+			{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      </header>
-    </div>
-  );
+      </header> */}
+			<Header></Header>
+			<Field></Field>
+			<Btn></Btn>
+		</div>
+	);
 }
 
+
+export { Header };
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,23 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Header } from './App';
 
 
-const text = "Hello"
-const elem = (
-  <div>
-    <h2 className='text'>World: {["awe",'qwe','123', text]}</h2>
-    <input type='text'></input>
-    <button tabIndex={0}>Click</button>
-  </div>
-);
+;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem,
   // <React.StrictMode>
-  //   <App />
+    <App />
   // </React.StrictMode>
 );
 
